39907ab7947d38d7f108da9d92e52dc37181de46
omerkocbil <omer-95@hotmail.com>
2022-06-18 23:54:22 +0300

    cleaning operation 2: remove clients with only 1 product view
    Do the second cleaning operation which is removing clients with only 1
    product view.

--- prep/cleaning.py
+++ prep/cleaning.py
@@ -7,5 +7,15 @@ data = pd.read_csv("../data/case_study_data_v1.csv",
 data['timestamp'] = pd.to_datetime(data['timestamp'], format='%Y-%m-%d %H:%M:%S')
 
 #first operation - remove client's duplicate product viewing
-data_v1 = data.drop_duplicates(subset = ['client', 'product_id'],
+data_op1 = data.drop_duplicates(subset = ['client', 'product_id'],
                                keep = 'first').reset_index(drop = True)
+
+#second operation - remove clients with only 1 product view
+client_data = data_op1.groupby('client')['product_id'].apply(list) \
+                  .reset_index(name='products')
+                  
+client_data['view_count'] = [len(product_list) 
+                             for product_list in client_data['products']]
+
+customers_to_be_removed = client_data[client_data['view_count'] == 1]['client'].to_list()
+data_op2 = data_op1[~data_op1['client'].isin(customers_to_be_removed)]
