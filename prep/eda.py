eb8352a6c0a065d40aa3c8964f1ff7aeab3fac4a
omerkocbil <omer-95@hotmail.com>
2022-06-18 20:53:53 +0300

    plot products distribution

--- prep/eda.py
+++ prep/eda.py
@@ -32,3 +32,24 @@ sns.histplot(client_diff_views)
 
 print(f"min: {min(client_diff_views)}, max: {max(client_diff_views)}, \
       mode: {stat.mode(client_diff_views)}, mean: {stat.mean(client_diff_views)}")
+
+#plot products distribution
+product_views = data['product_id'].value_counts()
+sns.histplot(product_views, binwidth=1)
+
+print(sorted(product_views))
+print(f"min: {min(product_views)}, max: {max(product_views)}, \
+      mode: {stat.mode(product_views)}, mean: {stat.mean(product_views)}")
+
+#remove client's same viewing
+product_data = data.groupby('product_id')['client'].apply(set) \
+                   .reset_index(name='clients')
+product_diff_views = [len(list) for list in product_data['clients'].to_list()]
+sns.histplot(product_diff_views, binwidth=1)
+
+print(f"min: {min(product_diff_views)}, max: {max(product_diff_views)}, \
+      mode: {stat.mode(product_diff_views)}, mean: {stat.mean(product_diff_views)}")
+
+#more than 25 viewed products
+product_diff_views_gt25 = [view for view in product_diff_views if view > 25]
+sns.histplot(product_diff_views_gt25, binwidth=25)
