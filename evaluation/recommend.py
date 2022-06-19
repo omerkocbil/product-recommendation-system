a257bde0bf199bee25e0760aa7b7a36c6ba74428
omerkocbil <omer-95@hotmail.com>
2022-06-19 18:46:52 +0300

    create recommender function
    Create recommender function. This function takes 2 parameter which are
    product_id and number of recommendations. Returns a list of tuples with
    recommended product and recommendation score.

---
+++ evaluation/recommend.py
@@ -0,0 +1,7 @@
+import pickle
+    
+with open("../model/word2vec_model.pkl", "rb") as f:
+    model = pickle.load(f)
+
+def recommend_products(product_id, n=15):
+    return model.wv.similar_by_word(product_id, topn=n)
