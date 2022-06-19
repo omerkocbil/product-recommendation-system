0875765747c93af3586b57781f72fbd71d9b440a
omerkocbil <omer-95@hotmail.com>
2022-06-19 16:14:18 +0300

    create word2vec model to pickle file
    Create word2vec model to pickle file which is word2vec_model.pkl in model
    folder

--- training/word2vec.py
+++ training/word2vec.py
@@ -8,3 +8,7 @@ with open("../data/word_vectors.pkl", "rb") as f:
 model = Word2Vec(sentences=train_word_vector, window=10, sg=1, hs=0, negative=20,
                  alpha=0.03, min_alpha=0.0007, vector_size=100, epochs=10, 
                  seed=24)
+
+#save the model
+with open("../model/word2vec_model.pkl", "wb") as f:
+    pickle.dump(model, f, protocol=pickle.HIGHEST_PROTOCOL)
