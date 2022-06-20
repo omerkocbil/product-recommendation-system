062c19e93edf2fd1f51a344d2b31a7f99a9cd4ea
omerkocbil <omer-95@hotmail.com>
2022-06-21 00:55:23 +0300

    change window parameter

--- code/training/word2vec.py
+++ code/training/word2vec.py
@@ -5,7 +5,7 @@ with open("data/word_vector.pkl", "rb") as f:
     word_vector = pickle.load(f)
 
 #train word2vec model
-model = Word2Vec(sentences=word_vector, window=10, sg=1, hs=0, negative=20,
+model = Word2Vec(sentences=word_vector, window=3, sg=1, hs=0, negative=20,
                  alpha=0.03, min_alpha=0.0007, vector_size=100, epochs=10, 
                  seed=24)
 
